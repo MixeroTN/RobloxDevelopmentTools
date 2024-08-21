b8ea6a536a61bf6320caff0d29cb99400da70639
Michał Gołkowski <shiro@mailo.com>
2024-08-21 17:11:59 +0200

    Update wally-auto-install.sh
    Co-authored-by: kardavx <kardaszjanojutubero@gmail.com>

--- wally-auto-install.sh
+++ wally-auto-install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Initial package installation
+echo "Fetching packages..."
+wally install
+echo "Exporting sourcemap..."
+rojo sourcemap default.project.json --output sourcemap.json
+echo "Exposing package types..."
+wally-package-types --sourcemap sourcemap.json Packages/
+
 # Create the post-merge script and write the commands to it
 cat << EOF > .git/hooks/post-merge
 #!/bin/bash
-echo "Running wally install"
+echo "Fetching packages..."
 wally install
+echo "Exporting sourcemap..."
+rojo sourcemap default.project.json --output sourcemap.json
+echo "Exposing package types..."
+wally-package-types --sourcemap sourcemap.json Packages/
 EOF
 
 # Make the post-merge script executable
